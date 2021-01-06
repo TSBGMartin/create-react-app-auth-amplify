7a1beb8a79602d3da61b50b272cc2d0cd430bd65
Craig Martin <craig.martin@solelife.com>
2021-01-06 11:14:28 -0500

    push demo

--- src/App.js
+++ src/App.js
@@ -11,19 +11,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <script>
+          window.intercomSettings = { app_id: "wvp4dx3s"  };
+        </script>
       </div>
     );
   }
